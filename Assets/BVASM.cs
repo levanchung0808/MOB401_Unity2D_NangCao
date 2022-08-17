f585b312fd481492e9f0ba3bdde7b519c7330bb9
LeVanChung19319 <86992800+LeVanChung19319@users.noreply.github.com>
2022-08-17 15:35:30 +0700

    trong buoi asm

---
+++ Assets/BVASM.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BVASM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("LevelGame");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT GAME");
+        Application.Quit();
+    }
+}
